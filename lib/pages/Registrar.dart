6489070fd8c5926f67f4b892488621180d95a307
MariaBernal23 <marbernalgamez8@gmail.com>
2023-10-23 07:13:09 -0600

    agregando dependencias

--- lib/pages/Registrar.dart
+++ lib/pages/Registrar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 
 class Registro extends StatelessWidget {
   Future<void> agregarUsuario(String idusuario, String nombre, String contrasena) async {
